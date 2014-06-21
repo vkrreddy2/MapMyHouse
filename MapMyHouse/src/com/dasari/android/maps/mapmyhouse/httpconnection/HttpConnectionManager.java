d794732b62f667ec29368fab113ff0cbbd9422db
Dasari Harikrishna <hariy6it826@gmail.com>
2014-06-21 18:24:59 +0530

    commit httpconnectin manager name changes

--- MapMyHouse/src/com/dasari/android/maps/mapmyhouse/httpconnection/HttpConnectionManager.java
+++ MapMyHouse/src/com/dasari/android/maps/mapmyhouse/httpconnection/HttpConnectionManager.java
@@ -1,11 +1,17 @@
 package com.dasari.android.maps.mapmyhouse.httpconnection;
 
+import org.apache.http.params.HttpParams;
+
 import android.content.Context;
 
 public class HttpConnectionManager {
 
 	private static HttpConnectionManager mInstance = null;
-	private HttpGetAsynctask mLocationRequestAsyncTask = null; 
+	private HttpGetAsynctask mHttpGetAsyncTask = null;
+	private HttpPostAsyncTask mHttpPostAsyncTask = null;
+	public static enum REQUEST_TYPE {
+		GET, POST
+	}
 	private HttpConnectionManager() {
 
 	}
@@ -17,16 +23,20 @@ public class HttpConnectionManager {
 		return mInstance;
 	}
 
-	public void makeRequest(Context context, String url, int requestID,
-			IOResponseListener listenr) {
-		mLocationRequestAsyncTask = new HttpGetAsynctask(
-				context, requestID, listenr);
-		mLocationRequestAsyncTask.execute(url);
-	}
-	
-	public void cancelrequest()
-	{
-		mLocationRequestAsyncTask.cancel(true);
+	public void makeRequest(Context context, String url, REQUEST_TYPE requestType,
+			int requestID, IOResponseListener listener, HttpParams params) throws IllegalArgumentException{
+		switch (requestType) {
+			case GET :
+					mHttpGetAsyncTask = new HttpGetAsynctask(context, requestID, listener);
+					mHttpGetAsyncTask.execute(url);
+				break;
+			case POST:
+					mHttpPostAsyncTask = new HttpPostAsyncTask(context, requestID, listener, params);
+					mHttpPostAsyncTask.execute(url);
+				break;
+			default :
+					throw new IllegalArgumentException("Request type method is not valid");
+		}
 	}
 
 	public interface IOResponseListener {
