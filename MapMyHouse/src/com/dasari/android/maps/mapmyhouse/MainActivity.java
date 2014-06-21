4d349ce4abfa70416fc07ea59b6e4ed8faed2373
Dasari Harikrishna <hariy6it826@gmail.com>
2014-06-22 02:13:26 +0530

    Added search button and actionbar transparent and location button position change

--- MapMyHouse/src/com/dasari/android/maps/mapmyhouse/MainActivity.java
+++ MapMyHouse/src/com/dasari/android/maps/mapmyhouse/MainActivity.java
@@ -4,8 +4,11 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.location.Geocoder;
 import android.location.Location;
 import android.net.Uri;
@@ -18,7 +21,9 @@ import android.view.MenuItem;
 //import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.Window;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.dasari.android.maps.mapmyhouse.location.MyLocationManager;
@@ -31,21 +36,23 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends Activity implements
-		OnMyLocationButtonClickListener, ILocationChangeListener, OnClickListener{
+public class MainActivity extends Activity
+		implements
+			OnMyLocationButtonClickListener,
+			ILocationChangeListener,
+			OnClickListener {
 
 	// Log tag
 	private static final String TAG = MainActivity.class.getCanonicalName();
-	
-	// Parcelable location details 
+
+	// Parcelable location details
 	public LocationDetails myDetails;
-	
+
 	// Extra Constant for passing to register acitivity..
 	private static final String LOACL_DETAILS_PARCEL = "com.android.dasari.myLocalDetails";
-	
+
 	private GoogleMap mGoogleMap;
 	private ImageView mNavigationButton;
-
 
 	private boolean mRegisterEnable;
 
@@ -55,23 +62,47 @@ public class MainActivity extends Activity implements
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		getWindow().requestFeature(Window.FEATURE_ACTION_BAR_OVERLAY);
 		setContentView(R.layout.activity_main);
 
+		ActionBar actionBar = getActionBar();
+		actionBar.setBackgroundDrawable(new ColorDrawable(Color
+				.parseColor("#90000000")));
+		actionBar.setStackedBackgroundDrawable(new ColorDrawable(Color
+				.parseColor("#80000000")));
+
+		MyLocationManager.getInstance().registerLocationListener(this);
+		MyLocationManager.getInstance().initialize(getApplicationContext());
+
+		myDetails = new LocationDetails();
 		mGoogleMap = ((MapFragment) getFragmentManager().findFragmentById(
 				R.id.map)).getMap();
 		mGoogleMap.setOnMyLocationButtonClickListener(this);
 		mGoogleMap.setMyLocationEnabled(true);
-		
-		mNavigationButton = (ImageView)findViewById(R.id.direction_button);
+
+		mGoogleMap.getUiSettings().setZoomControlsEnabled(false);
+
+		View locationButton = ((View) findViewById(1).getParent())
+				.findViewById(2);
+
+		// and next place it, for exemple, on bottom right (as Google Maps app)
+		RelativeLayout.LayoutParams rlp = (RelativeLayout.LayoutParams) locationButton
+				.getLayoutParams();
+		// position on right bottom
+		rlp.addRule(RelativeLayout.ALIGN_PARENT_TOP, 0);
+		rlp.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+		rlp.setMargins(0, 0, 30, 30);
+
+		mNavigationButton = (ImageView) findViewById(R.id.direction_button);
 		mNavigationButton.setOnClickListener(this);
-/*		LatLng bangalore = new LatLng(12.971599, 77.594563);
-		mGoogleMap.setOnMyLocationButtonClickListener(this);
-		mGoogleMap.setMyLocationEnabled(true);
-		mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(bangalore, 13));
-		mGoogleMap.addMarker(new MarkerOptions().title("Bangalore")
-				.snippet("The most populous city in India.")
-				.position(bangalore));
-*/
+		/*
+		 * LatLng bangalore = new LatLng(12.971599, 77.594563);
+		 * mGoogleMap.setOnMyLocationButtonClickListener(this);
+		 * mGoogleMap.setMyLocationEnabled(true);
+		 * mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(bangalore,
+		 * 13)); mGoogleMap.addMarker(new MarkerOptions().title("Bangalore")
+		 * .snippet("The most populous city in India.") .position(bangalore));
+		 */
 		/*
 		 * map.setOnMapClickListener(new OnMapClickListener() {
 		 * 
@@ -114,7 +145,7 @@ public class MainActivity extends Activity implements
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.register) {
-			 registerLocation();
+			registerLocation();
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
@@ -124,13 +155,13 @@ public class MainActivity extends Activity implements
 		MyLocationManager.getInstance().getLocation(getApplicationContext());
 		return false;
 	}
-	
+
 	@Override
 	protected void onStart() {
 		// TODO Auto-generated method stub
 		super.onStart();
 	}
-	
+
 	@Override
 	protected void onResume() {
 		super.onResume();
@@ -138,7 +169,6 @@ public class MainActivity extends Activity implements
 		MyLocationManager.getInstance().initialize(getApplicationContext());
 		MyLocationManager.getInstance().connectToLocationService();
 
-		
 	};
 	@Override
 	protected void onStop() {
@@ -149,67 +179,72 @@ public class MainActivity extends Activity implements
 
 	@Override
 	public void onLocationChange(Location location) {
-		 myDetails = new LocationDetails();
-		 myDetails.setLatitude(location.getLatitude());
-		 myDetails.setLongitude(location.getLongitude());
-		 double localLat = myDetails.getLatitude();
-		 double localLong = myDetails.getLongitude();
-		 LatLng myloc = new LatLng(localLat, localLong);
-		 mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myloc, 13));
-		 mGoogleMap.addMarker(new MarkerOptions().title("My Location")
-		 .snippet("This is my location").position(myloc));
-		 //TODO random code.. generaiton.
-		 
-		 myDetails.setUniqueKey("rami99999");
-		 
-         new DownlaodAddressFormLatLog().execute(localLat, localLong);
-//		 MyLocationDao myLocDao = new MyLocationDao();
-//		 myLocDao.setMyHouseID("KA9999");
-//		 myLocDao.setMyLocationLatitude(mLatitude);
-//		 myLocDao.setMyLocationLongitude(mLongitude);
-//		 myLocDao.setMyAddress("Flat# 311/A, Deverabisinahalli, Bangalore");
-//		 
-//		 MapMyHouseDBHelper.getInstance(getApplicationContext()).openDatabase();
-//		 MapMyHouseDBHelper.getInstance(getApplicationContext()).insert(myLocDao);
-//		 MapMyHouseDBHelper.getInstance(getApplicationContext()).close();
-//	 
-	
+		myDetails = new LocationDetails();
+		myDetails.setLatitude(location.getLatitude());
+		myDetails.setLongitude(location.getLongitude());
+		double localLat = myDetails.getLatitude();
+		double localLong = myDetails.getLongitude();
+		LatLng myloc = new LatLng(localLat, localLong);
+		mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myloc, 13));
+		mGoogleMap.addMarker(new MarkerOptions().title("My Location")
+				.snippet("This is my location").position(myloc));
+		// TODO random code.. generaiton.
+
+		myDetails.setUniqueKey("rami99999");
+
+		new DownlaodAddressFormLatLog().execute(localLat, localLong);
+		// MyLocationDao myLocDao = new MyLocationDao();
+		// myLocDao.setMyHouseID("KA9999");
+		// myLocDao.setMyLocationLatitude(mLatitude);
+		// myLocDao.setMyLocationLongitude(mLongitude);
+		// myLocDao.setMyAddress("Flat# 311/A, Deverabisinahalli, Bangalore");
+		//
+		// MapMyHouseDBHelper.getInstance(getApplicationContext()).openDatabase();
+		// MapMyHouseDBHelper.getInstance(getApplicationContext()).insert(myLocDao);
+		// MapMyHouseDBHelper.getInstance(getApplicationContext()).close();
+		//
+
 	}
 
-	private class DownlaodAddressFormLatLog extends AsyncTask<Double, Void, Boolean>{
+	private class DownlaodAddressFormLatLog
+			extends
+				AsyncTask<Double, Void, Boolean> {
 
 		@Override
 		protected Boolean doInBackground(Double... params) {
-			 // Used to get the NEAREST address of the location pointed by
-			 // longitude and latitude.
-			 Geocoder myLoc = new Geocoder(MainActivity.this, Locale.getDefault());
-			 List<android.location.Address> myadd;
+			// Used to get the NEAREST address of the location pointed by
+			// longitude and latitude.
+			Geocoder myLoc = new Geocoder(MainActivity.this,
+					Locale.getDefault());
+			List<android.location.Address> myadd;
 			try {
 				myadd = myLoc.getFromLocation(params[0], params[1], 1);
-				 if (myadd !=null && myadd.size()>0){
-					 for (android.location.Address add : myadd){
-						 myDetails.setLocality(add.getLocality());
-						 myDetails.setAdmin(add.getAdminArea());
-						 myDetails.setPostalCode(add.getPostalCode());
-						 myDetails.setCountry(add.getCountryName());
-					 }
-					    
-				 }
-		    return true;
+				if (myadd != null && myadd.size() > 0) {
+					for (android.location.Address add : myadd) {
+						myDetails.setLocality(add.getLocality());
+						myDetails.setAdmin(add.getAdminArea());
+						myDetails.setPostalCode(add.getPostalCode());
+						myDetails.setCountry(add.getCountryName());
+					}
+
+				}
+				return true;
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				Log.e(TAG, "IO exception at GeoCoder.. probably system error");
 				e.printStackTrace();
 				return false;
 			}
-			
+
 		}
-		
+
 		@Override
 		protected void onPostExecute(Boolean result) {
 			super.onPostExecute(result);
 			if (!result)
-				Toast.makeText(MainActivity.this, R.string.geocoder_connection_error, Toast.LENGTH_LONG).show();
+				Toast.makeText(MainActivity.this,
+						R.string.geocoder_connection_error, Toast.LENGTH_LONG)
+						.show();
 			mRegisterEnable = true;
 			invalidateOptionsMenu();
 		}
@@ -217,47 +252,51 @@ public class MainActivity extends Activity implements
 	/**
 	 * To register location and showing user his unique key along with latitude
 	 * longitude and prompting user to fill address (optional).
-	 * @param latitude Latitude value of the user location.
-	 * @param longitude Longitude value of the user location.
+	 * 
+	 * @param latitude
+	 *            Latitude value of the user location.
+	 * @param longitude
+	 *            Longitude value of the user location.
 	 * @param key
 	 */
 	private void registerLocation() {
 		Intent registerLocation = new Intent(this, RegisterMyLocation.class);
-		//Bundle bundle = new Bundle();
-		//bundle.putParcelable(LOACL_DETAILS_PARCEL, myDetails);
-		registerLocation.putExtra(LOACL_DETAILS_PARCEL, (Parcelable)myDetails);
-		//registerLocation.putExtras(bundle);
+		// Bundle bundle = new Bundle();
+		// bundle.putParcelable(LOACL_DETAILS_PARCEL, myDetails);
+		registerLocation.putExtra(LOACL_DETAILS_PARCEL, (Parcelable) myDetails);
+		// registerLocation.putExtras(bundle);
 
 		startActivity(registerLocation);
 	}
 
-	
 	@Override
 	public void onServiceConnected(Bundle args) {
-		Toast.makeText(getApplicationContext(), "Location service connected", Toast.LENGTH_SHORT).show();
+		Toast.makeText(getApplicationContext(), "Location service connected",
+				Toast.LENGTH_SHORT).show();
 	}
 
 	@Override
 	public void onServiceDisconnected() {
-		Toast.makeText(getApplicationContext(), "Location service disconnected", Toast.LENGTH_SHORT).show();		
+		Toast.makeText(getApplicationContext(),
+				"Location service disconnected", Toast.LENGTH_SHORT).show();
 	}
 
 	@Override
 	public void onServiceConnectionFailed(ConnectionResult arg0) {
-		Toast.makeText(getApplicationContext(), "Location service connected Failed", Toast.LENGTH_SHORT).show();		
+		Toast.makeText(getApplicationContext(),
+				"Location service connected Failed", Toast.LENGTH_SHORT).show();
 	}
 
 	@Override
 	public void onClick(View v) {
-		switch(v.getId())
-		{
-			case R.id.direction_button:
+		switch (v.getId()) {
+			case R.id.direction_button :
 
-						 Intent navigation = new Intent(Intent.ACTION_VIEW);
-						 navigation.setData(Uri.parse("geo:0,0?q=12.927204,77.686455 (" + "My House" + ")"));
-						 startActivity(navigation);
-						 
-						 
+				Intent navigation = new Intent(Intent.ACTION_VIEW);
+				navigation.setData(Uri.parse("geo:0,0?q=12.927204,77.686455 ("
+						+ "My House" + ")"));
+				startActivity(navigation);
+
 		}
 	}
 }
